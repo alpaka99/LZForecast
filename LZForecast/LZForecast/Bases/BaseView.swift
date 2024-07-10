611bfa8a3b0e0cf29c2a3608d0e5c9b5c2058c75
Lets <ruvan123@naver.com>
2024-07-10 17:37:00 +0900

    Feat: BaseView 코드 구현

---
+++ LZForecast/LZForecast/Bases/BaseView.swift
@@ -0,0 +1,30 @@
+//
+//  BaseView.swift
+//  LZForecast
+//
+//  Created by user on 7/10/24.
+//
+
+import UIKit
+
+class BaseView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        
+        configureHierarchy()
+        configureLayout()
+        configureUI()
+    }
+    
+    @available(iOS, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    internal func configureHierarchy() { }
+    
+    internal func configureLayout() { }
+    
+    internal func configureUI() { }
+}
