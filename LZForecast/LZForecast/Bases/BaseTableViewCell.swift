435f30769993edd055e6fe5d1887fd23224c798b
Lets <ruvan123@naver.com>
2024-07-11 16:32:14 +0900

    Feat: BaseTableViewCell 구현

---
+++ LZForecast/LZForecast/Bases/BaseTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  BaseTableViewCell.swift
+//  LZForecast
+//
+//  Created by user on 7/11/24.
+//
+
+import UIKit
+
+class BaseTableViewCell: UITableViewCell {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        configureHierarchy()
+        configureLayout()
+        configureUI()
+    }
+    
+    @available(iOS, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    internal func configureHierarchy() { }
+    
+    internal func configureLayout() { }
+    
+    internal func configureUI() { }
+}
